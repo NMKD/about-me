53400f1540e16fbbe064553b71c47ca4cc902598
kristina <kistina@localhost>
2021-12-20 16:49:11 +0300

    добавлены атрибуты target blank

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import './css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import './js/app';
-import './componentProfessional/js/scripts';
 
 // TODO: write your code in app.js
