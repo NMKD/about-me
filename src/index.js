c19337ad78df0e19c67aa1c8fa3100742ccb4e77
kristina <kistina@localhost>
2021-12-20 17:20:16 +0300

    import min css'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/extensions */
 import './css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import './js/app';
 
 // TODO: write your code in app.js
